62e4ab1d2de3d46ff2616ed45ecfa86fb50ba7c2
MoriyaEster <moriyaeo100@gmail.com>
2024-01-17 18:39:28 +0200

    connection form

---
+++ components/ConnectionForm.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+export class UserForm extends Component {
+  state = {
+    step: 1,
+    employee: 0,
+    employer: 0,
+    userID: '',
+    password: ''
+  };
+
+  // Proceed to next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
+
+  // Go back to prev step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  };
+
+  // Handle fields change
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value });
+  };
+
+  render() {
+    const { step } = this.state;
+    const { userID, password} = this.state;
+    const values = { userID, password};
+
+    switch (step) {
+      case 1:
+        return (
+          <UserType
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <FormPersonalDetails
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 3:
+        return (
+          <Confirm
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            values={values}
+          />
+        );
+      case 4:
+        return <Success />;
+      default:
+        (console.log('This is a multi-step form built with React.'))
+    }
+  }
+}
+
+export default UserForm;
