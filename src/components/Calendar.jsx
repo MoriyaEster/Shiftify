e499866b7fd6f48c71df3d0fd72eaf2830dad893
ofekats <ofekats@gmail.com>
2024-01-21 11:10:45 +0200

    Calendar hebrew

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import heLocale from '@fullcalendar/core/locales/he';
+
+export class Calendar extends Component {
+    render() {
+        return (
+            <div>
+                <FullCalendar
+                    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                    initialView={"dayGridMonth"}
+                    headerToolbar={{
+                        start: "today prev,next",
+                        center: "title",
+                        end: "dayGridMonth,timeGridWeek,timeGridDay",
+                    }}
+                    height={"90vh"}
+                    locales={[heLocale]} // Set the locale to Hebrew
+                    locale="he" // Specify the Hebrew locale
+                />
+            </div>
+        );
+    }
+}
+
+export default Calendar;
