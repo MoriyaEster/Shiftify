3074f32af9e71aba449efbba8ceaacdc7deabb3c
ofekats <ofekats@gmail.com>
2024-01-31 16:37:15 +0200

    with HomePageEmployee

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ConnectionForm } from './components/ConnectionForm';
 import './App.css';
 import { RegisteryForm } from './components/RegisteryForm';
 import { SelectShifts } from './components/SelectShifts';
+import { HomePageEmployee } from './components/HomePageEmployee';
 import { createBrowserRouter,BrowserRouter as Router, Routes, Route, BrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'; // Import only here
 
 const router = createBrowserRouter(
