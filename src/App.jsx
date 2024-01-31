79f893fcb9b2e679b9478516aa540d690dc44e8f
ofekats <ofekats@gmail.com>
2024-01-31 17:00:04 +0200

    with ProfileEmployee

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import { RegisteryForm } from './components/RegisteryForm';
 import { SelectShifts } from './components/SelectShifts';
 import { HomePageEmployee } from './components/HomePageEmployee';
+import { ProfileEmployee } from './components/ProfileEmployee';
 import { createBrowserRouter,BrowserRouter as Router, Routes, Route, BrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'; // Import only here
 
 const router = createBrowserRouter(
@@ -13,6 +14,8 @@ const router = createBrowserRouter(
       <Route path="/Registery" element={<RegisteryForm />} />
       <Route path="/SelectShifts" element={<SelectShifts />} />
       <Route path="/HomePageEmployee" element={<HomePageEmployee />} />
+      <Route path="/ProfileEmployee" element={<ProfileEmployee />} />
+
     </Route>
   )
 )
