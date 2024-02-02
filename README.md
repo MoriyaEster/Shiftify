ff2ccfc16f71bb4c27c9896b9c5aa4c91dc88910
zachibs <zachibsn@gmail.com>
2024-02-02 12:38:03 +0000

    backend initial

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## SETUP
+### Frontend
+* `sudo apt-get update -y`
+* `apt-get install -y locales`
+* `sudo apt-get install curl -y`
+* `curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\ 
+sudo apt-get install -y nodejs`
+* `cd frontend`
+* `npm i`
+* `npm run dev -o`
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+### Backend
+* 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,9 @@ import { ConnectionForm } from './components/ConnectionForm';
 import './App.css';
 import { RegisteryForm } from './components/RegisteryForm';
 import { SelectShifts } from './components/SelectShifts';
-import { HomePage } from './components/HomePage';
-import { Profile } from './components/Profile';
-import { WorkersManagement } from './components/WorkersManagement';
+// import { HomePage } from './components/HomePage';
+// import { Profile } from './components/Profile';
+// import { WorkersManagement } from './components/WorkersManagement';
 import { ShiftManagement } from './components/ShiftManagement';
 
 import { createBrowserRouter,BrowserRouter as Router, Routes, Route, BrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
@@ -18,9 +18,9 @@ const router = createBrowserRouter(
         <Route path=":employer" element={<RegisteryForm />}/>
       </Route>
       <Route path="/SelectShifts" element={<SelectShifts />} />
-      <Route path="/HomePage" element={<HomePage />} />
-      <Route path="/Profile" element={<Profile />} /> 
-      <Route path="/WorkersManagement" element={<WorkersManagement />} />
+      {/* <Route path="/HomePage" element={<HomePage />} /> */}
+      {/* <Route path="/Profile" element={<Profile />} />  */}
+      {/* <Route path="/WorkersManagement" element={<WorkersManagement />} /> */}
       <Route path="/ShiftManagement" element={<ShiftManagement />} />
     </Route>
   )
