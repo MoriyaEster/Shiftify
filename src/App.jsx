5840b6b958b1cd5d62f0cf8d5a04736ce0d6bde3
MoriyaEster <moriyaeo100@gmail.com>
2024-02-04 13:29:30 +0200

    with stopwach

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { RegisteryForm } from './components/RegisteryForm';
 import { SelectShifts } from './components/SelectShifts';
 import { HomePage } from './components/HomePage';
 import { Profile } from './components/Profile';
-import { Entry } from './components/Entry';
 // import { WorkersManagement } from './components/WorkersManagement';
 import { ShiftManagement } from './components/ShiftManagement';
 import { ApprovedShift } from './components/ApprovedShift';
@@ -21,7 +20,6 @@ const router = createBrowserRouter(
       </Route>
       <Route path="/SelectShifts" element={<SelectShifts />} />
       <Route path="/HomePage" element={<HomePage />} />
-      <Route path="/HomePage/Entry" element={<Entry />}/>
       <Route path="/Profile" element={<Profile />} /> 
       {/* <Route path="/WorkersManagement" element={<WorkersManagement />} /> */}
       <Route path="/ShiftManagement" element={<ShiftManagement />} />
