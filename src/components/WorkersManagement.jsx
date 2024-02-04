bb8ab5a545cdc2d1e0180c4195e3a506d4216b6c
Ariel Dvori <Arieldv102@gmail.com>
2024-02-04 22:13:49 +0200

    nothing yet

---
+++ src/components/WorkersManagement.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '/src/App.css'
+
+function page() {
+  const [selects,setSelects]=Component();  
+  return(
+    <div>
+      <select>
+        <h1>שלום</h1>
+        <option> תפוח </option>
+      </select>
+    </div>
+  );
+}  
+
+
+export default page();
+//export default WorkersManagement;
