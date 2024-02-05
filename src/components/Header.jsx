cd940c6aa613283bd77108cc7eefdeb108ae5981
ofekats <ofekats@gmail.com>
2024-02-05 15:21:07 +0200

    log out wuthout error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import { useUser } from '/src/UserContext.jsx';
 import '/src/App.css';
 
-export class Header extends Component {
+export const Header = () => {
+  const { handleLogout } = useUser();
 
-
-
-  logout = () => {
-    const { handleLogout } = useUser();
+  const logout = () => {
     handleLogout();
-  }
+  };
 
-
-  render() {
-    return (
-      <>
-        <div className="img-container">
-          <Link to="/HomePage">
-            <img className="img-backbutton" src="src/photos/backbutton.jpg" alt="backbutton" />
-          </Link>
-          <img className="img-logo" src="src/photos/logo.png" alt="logo" />
-          <Link to="/Profile">
-            <img className="img-profile" src="src/photos/profile.jpg" alt="profile" />
-          </Link>
-          <Link to="/">
-            <Button
-              color="primary"
-              variant="contained"
-              onClick={() => {
-                this.logout();
-              }}
-            > התנתקות</Button>
-          </Link>
-        </div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div className="img-container">
+        <Link to="/HomePage">
+          <img className="img-backbutton" src="src/photos/backbutton.jpg" alt="backbutton" />
+        </Link>
+        <img className="img-logo" src="src/photos/logo.png" alt="logo" />
+        <Link to="/Profile">
+          <img className="img-profile" src="src/photos/profile.jpg" alt="profile" />
+        </Link>
+        <Link to="/">
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={() => {
+              logout();
+            }}
+          >
+            התנתקות
+          </Button>
+        </Link>
+      </div>
+    </>
+  );
+};
 
 export default Header;
