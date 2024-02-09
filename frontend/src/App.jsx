2b8cf027b1e0284e4944d5209af74384b1f8ae00
zachibs <zachibsn@gmail.com>
2024-02-09 15:14:11 +0000

    # fixing the pull made from main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,12 @@ import { RegisteryForm } from './components/RegisteryForm';
 import { SelectShifts } from './components/SelectShifts';
 import { HomePage } from './components/HomePage';
 import { Profile } from './components/Profile';
+import { EditEmployee} from './components/EditEmployee';
 import { WorkersManagement } from './components/WorkersManagement';
+import { WorkHoursManagement } from './components/WorkHoursManagement';
 import { ShiftManagement } from './components/ShiftManagement';
+import { ApprovedShift } from './components/ApprovedShift';
+import { UserProvider } from './UserContext';
 
 import { createBrowserRouter,BrowserRouter as Router, Routes, Route, BrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 
@@ -20,15 +24,20 @@ const router = createBrowserRouter(
       <Route path="/SelectShifts" element={<SelectShifts />} />
       <Route path="/HomePage" element={<HomePage />} />
       <Route path="/Profile" element={<Profile />} /> 
-      <Route path="/WorkersManagement" element={<WorkersManagement />} />
+      <Route path="/EditEmployee" element={<EditEmployee />} /> 
+      <Route path="/WorkersManagement" element={<WorkersManagement />} /> 
+      <Route path="/WorkHoursManagement" element={<WorkHoursManagement />} /> 
       <Route path="/ShiftManagement" element={<ShiftManagement />} />
+      <Route path="/ApprovedShift" element={<ApprovedShift />} />
     </Route>
   )
 )
 
 function App() {
   return (
+    <UserProvider>
     <RouterProvider router={router} />
+    </UserProvider>
   );
 }
 
