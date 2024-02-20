677150cc87bcb093b9ce90821c111d0a14ffa3cc
ofekats <ofekats@gmail.com>
2024-02-20 14:56:40 +0200

    sending gmail

--- frontend/src/components/ApprovedShift.jsx
+++ frontend/src/components/ApprovedShift.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
@@ -6,53 +6,49 @@ import interactionPlugin from "@fullcalendar/interaction";
 import heLocale from '@fullcalendar/core/locales/he';
 import { Header } from './Header';
 import { useUser } from '/src/UserContext.jsx';
-import { useNavigate } from 'react-router-dom';
 import UserConnectionChecker from './UserConnectionChecker';
 import axios from 'axios';
 import * as links from '/src/axios-handler.jsx';
-
-const buttonClass = 'shift-button';
+import Button from '@mui/material/Button';
+import emailjs from 'emailjs-com';
+import { ModelPopUp } from './ModelPopUp';
 
 export const ApprovedShift = () => {
-
-  //get events from backend!!!!
-    //needs to be end:"2024-02-09T18:00:00" start:"2024-02-09T13:00:00" title:"noon Shift - 2024-02-09" 
-  //save userID
-  const { handleUserId ,handleWorkPlace } = useUser();
+  // save userID
+  const { handleUserId, handleWorkPlace, handleUserEmail, handleUserName } = useUser();
   const [userID, setUserID] = useState(handleUserId());
   const [WorkPlace, setWorkPlace] = useState(handleWorkPlace());
+  const [userEmail, setUserEmail] = useState(handleUserEmail());
+  const [userName, setUserName] = useState(handleUserName());
   const [events, setEvents] = useState([]);
-  
+
+  //popup
+  const [showModal, setShowModal] = useState(false);
+  const [status, setStatus] = useState(null);
+  const [contentPOPUP, setContentPOPUP] = useState(null);
+
   const handeljson = (jsondata) => {
-    // const eventsArray = JSON.parse(jsondata);
-      // Modify the date format in the array
-      const formattedArray = jsondata.map((event) => ({
-        title: event.title,
-        start: new Date(event.start),
-        end: new Date(event.end),
-        date: event.date,
-        type: event.type,
-        userID: event.username,
-        workPlace: event.workplace,
-      }));
-      setEvents(formattedArray);
-      console.log("events from json: ", formattedArray);
-      console.log("events: ", events);
+    const formattedArray = jsondata.map((event) => ({
+      title: event.title,
+      start: new Date(event.start),
+      end: new Date(event.end),
+      date: event.date,
+      type: event.type,
+      userID: event.username,
+      workPlace: event.workplace,
+    }));
+    setEvents(formattedArray);
+    console.log("events from json: ", formattedArray);
+    console.log("events: ", events);
   }
 
-  //get  info from backend
   useEffect(() => {
-    // Fetch events for the user
-    // const json  = '[{"title":"morning Shift - 2024-02-07","start":"2024-02-07T08:00:00","end":"2024-02-07T13:00:00","date":"2024-02-07","type":"morning","userID":"54335","WorkPlace":"Workplace 2"},{"title":"noon Shift - 2024-02-07","start":"2024-02-07T13:00:00","end":"2024-02-07T18:00:00","date":"2024-02-07","type":"noon","userID":"54335","WorkPlace":"Workplace 2"},{"title":"morning Shift - 2024-02-08","start":"2024-02-08T08:00:00","end":"2024-02-08T13:00:00","date":"2024-02-08","type":"morning","userID":"54335","WorkPlace":"Workplace 2"},{"title":"noon Shift - 2024-02-09","start":"2024-02-09T13:00:00","end":"2024-02-09T18:00:00","date":"2024-02-09","type":"noon","userID":"54335","WorkPlace":"Workplace 2"},{"title":"noon Shift - 2024-02-10","start":"2024-02-10T13:00:00","end":"2024-02-10T18:00:00","date":"2024-02-10","type":"noon","userID":"54335","WorkPlace":"Workplace 2"}]'
-    // handeljson(json);
     const fetchUserEvents = async () => {
       try {
         const response = await axios.get(links.url_approved_shifts + `?userID=${userID}&WorkPlace=${WorkPlace}`);
         console.log("response:", response);
-        // handeljson(response.data.events);
         if (response.status === 200) {
           console.log("Data fetched successfully:", response.data);
-          // Update state or perform other actions with the data
           handeljson(response.data.docs);
         } else {
           console.error(`Unexpected status code: ${response.status}`);
@@ -61,9 +57,93 @@ export const ApprovedShift = () => {
         console.error('Error fetching user events:', error);
       }
     };
-    // Call the fetchUserEvents function
     fetchUserEvents();
   }, [userID]);
+
+  const handleSendMail = async () => {
+    const formattedEvents = events
+  .filter((event) => {
+    const eventDate = new Date(event.date);
+    const currentDate = new Date();
+    return (
+      eventDate.getMonth() === currentDate.getMonth() &&
+      eventDate.getFullYear() === currentDate.getFullYear()
+    );
+  })
+  .map((event) => ({
+    date: event.date,
+    type: event.type,
+    start: event.start.toLocaleTimeString(),
+    end: event.end.toLocaleTimeString(),
+    workPlace: event.workPlace,
+  }));
+
+let eventsHTML;
+if (formattedEvents.length > 0) {
+  eventsHTML = `
+    <table border="1">
+      <tr>
+        <th>Date</th>
+        <th>Type</th>
+        <th>Start</th>
+        <th>End</th>
+        <th>Workplace</th>
+      </tr>
+      ${formattedEvents
+        .map(
+          (event) => `
+            <tr>
+              <td>${event.date}</td>
+              <td>${event.type}</td>
+              <td>${event.start}</td>
+              <td>${event.end}</td>
+              <td>${event.workPlace}</td>
+            </tr>
+          `
+        )
+        .join('')}
+    </table>`;
+} else {
+  eventsHTML = 'There are no shifts this month.';
+}
+    console.log('formattedEvents:', formattedEvents);
+    try {
+      const response = await emailjs.send(
+        'service_g8ko9ga', 
+        'template_uvs2lzw', 
+        {
+          to_email: userEmail,
+          subject: 'משמרות חודשיות',
+          userName: userName,
+          eventsHTML: eventsHTML,
+        },
+        'iJnFNnGWJKF7hbMSK'
+      );
+
+      console.log('Email sent successfully:', response);
+      if(response.status === 200)
+        {
+          setStatus(200);
+          setContentPOPUP("המשמרות נשלחו בהצלחה");
+          setShowModal(true);
+        }
+    } catch (error) {
+      console.error('Error sending email:', error);
+        setStatus(500);
+        setContentPOPUP("תקלה בהגשת משמרות");
+        setShowModal(true);
+    }
+
+    console.log("Sending email to:", userEmail);
+  };
+
+  const handleGoogleCalendar = () => {
+    if (userEmail.endsWith('@gmail.com')) {
+      console.log("Connecting events to Google Calendar");
+    } else {
+      console.log("User does not have a Gmail address");
+    }
+  };
 
   return (
     <div>
@@ -71,6 +151,16 @@ export const ApprovedShift = () => {
       <UserConnectionChecker />
       <h1>משמרות מאושרות</h1>
       <h4>{WorkPlace} :מקום העבודה הנבחר </h4>
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={handleSendMail}
+      > שתף עם המייל (gmail)</Button>
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={handleGoogleCalendar}
+      > שתף עם יומן גוגל</Button>
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView={"timeGridWeek"}
@@ -80,16 +170,16 @@ export const ApprovedShift = () => {
           end: "dayGridMonth,timeGridWeek,timeGridDay",
         }}
         height={"70vh"}
-        //hebrew
         locales={[heLocale]}
         locale="he"
         events={events}
         eventContent={({ event }) => (
           <>
-              <div style={{ fontSize: '12px' }} dangerouslySetInnerHTML={{ __html: event.title }}></div>
+            <div style={{ fontSize: '12px' }} dangerouslySetInnerHTML={{ __html: event.title }}></div>
           </>
-      )}
+        )}
       />
+      <ModelPopUp show={showModal} onClose={() => setShowModal(false)} status={status} content={contentPOPUP}/>
     </div>
   );
 };
